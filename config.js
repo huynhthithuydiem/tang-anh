070c25f3f9b209f609e41a9425fc6897895e6f83
huynhthithuydiem <87219524+huynhthithuydiem@users.noreply.github.com>
2021-07-10 12:44:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Anhhh Tiếnnn ơiiii!',
+    introDesc: `Em hong thích ăn cá
+    tại vì cá hơi tanh
+    em chỉ thích ăn anh
+    tại vì anh vười miệnggggg`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Nếu sốt cao thì gọi xe cấp cứu',
+    desc: 'Còn muốn yêu em thì chờ hết dịch em đến nhóooooo ',
+    btnYes: 'Vẫn cứ là thích em rùi  <33',
+    btnNo: 'Không yêu, em gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lướt dating lại gặp anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Em biết rùi nka, dễ gì qua mắt em 😘😘',
+    messDesc: 'Thủ Đức nhen, hok gặp nghỉ chơi :<.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100010680482898' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
